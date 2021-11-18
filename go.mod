be5e01a0494ed68c8c33c762ac44b15c490f7d78
panyi <panyi@digifinex.com>
2021-11-18 16:28:58 +0800

    优化 http

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ayher/ayhertool
 
 go 1.13
 
-require github.com/go-playground/validator/v10 v10.9.0
+require github.com/pkg/errors v0.9.1
--- httpRequest/http.go
+++ httpRequest/http.go
@@ -1,14 +1,43 @@
 package httpRequest
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"strings"
+	"time"
 )
 
-func GetMap(url string) (map[string]interface{},error) {
-	resp,err:=http.Get(url)
+var h = &HttpRequest{
+	Transport:&http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          50,
+		IdleConnTimeout:       90 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	},
+}
+
+type HttpRequest struct {
+	Transport *http.Transport
+}
+
+func GetMap(url string)(map[string]interface{},error){
+	return h.getMap(url)
+}
+func (self *HttpRequest)getMap(url string) (map[string]interface{},error) {
+	client:=&http.Client{}
+	client.Transport=self.Transport
+	resp,err:=client.Get(url)
+
 	if err!=nil{
 		return nil, err
 	}
@@ -27,11 +56,17 @@ func GetMap(url string) (map[string]interface{},error) {
 }
 
 func PostMap(url string,req interface{})(map[string]interface{},error){
+	return h.postMap(url ,req )
+}
+func (self *HttpRequest)postMap(url string,req interface{})(map[string]interface{},error){
 	b,err:=json.Marshal(req)
 	if err!=nil{
 		return nil,err
 	}
-	resp, err:=http.Post(url,"application/json",strings.NewReader(string(b)))
+
+	client:=&http.Client{}
+	client.Transport=self.Transport
+	resp, err:=client.Post(url,"application/json",strings.NewReader(string(b)))
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
